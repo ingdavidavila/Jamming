f788a5cb27f9ba560ee750e84e1ba37d86467e23
unknown <ingdavidavila@hotmail.com>
2022-06-19 17:45:37 -0500

    second commit

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './Playlist.css';
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component{
     render(){
         return(
             <div className="Playlist">
-            <input value="New Playlist"/>
-            {/* <TrackList/> */}
+            <input defaultValue={ "New Playlist" }/>
+            <TrackList/>
             <button class="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
