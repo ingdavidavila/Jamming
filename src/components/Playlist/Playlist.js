b759742b1d8cf94e65f29224d92831d8c560ecec
unknown <ingdavidavila@hotmail.com>
2022-06-25 16:47:58 -0500

    second day commit

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,12 +3,14 @@ import './Playlist.css';
 import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component{
+ 
+    
     render(){
         return(
             <div className="Playlist">
-            <input defaultValue={ "New Playlist" }/>
-            <TrackList/>
-            <button class="Playlist-save">SAVE TO SPOTIFY</button>
+            <input defaultValue={"New Playlist"}/>
+            <TrackList tracks={this.props.playlistTracks} /> 
+            <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
     }
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,8 +1,10 @@
 import React from "react";
 import './SearchResults.css';
-import TrackList from "../TrackList/TrackList";
+import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component{
+    
+
     render(){
         return(
             <div className="SearchResults">
