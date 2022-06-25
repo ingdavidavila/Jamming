0ea41c2b79d7815e8eed298cd83620e357857463
unknown <ingdavidavila@hotmail.com>
2022-06-25 17:55:32 -0500

    second day second push

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,10 +17,29 @@ class App extends React.Component {
       {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
       {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
       playlistName: "",
-      playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-      {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+      playlistTracks: [{name: 'playlistname1', artist: 'playlistartist1', album: 'playlistalbum1', id: 4},
+      {name: 'playlistname2', artist: 'playlistartist2', album: 'playlistalbum2', id: 5},
+      {name: 'playlistname3', artist: 'playlistartist3', album: 'playlistalbum3', id: 6}]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
     }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+    tracks= tracks.filter(currentTrack => currentTrack.id !== track.id); 
+     
+    
+    this.setState({playlistTracks: tracks});
   }
   
   render(){
@@ -30,9 +49,12 @@ class App extends React.Component {
        <div className="App">
         <SearchBar/> 
         <div className="App-playlist">
-        <SearchResults searchResults={this.state.searchResults}/>
+        <SearchResults searchResults={this.state.searchResults}
+        onAdd={this.addTrack}
+        />
         <Playlist playlistName={this.state.playlistName} 
-                  playlistTracks={this.state.playlistTracks}/>
+                  playlistTracks={this.state.playlistTracks}
+                  onRemove={this.removeTrack}/>
         </div>
       </div>
       </div>
